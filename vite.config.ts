994dbf80ef9f6ff32882c4a6fe90b307caeecf41
wuwenx <376679129@qq.com>
2025-03-11 22:48:31 +0800

    ci: 添加部署工作流并配置 Vite 基础路径
    - 新增 GitHub Actions 部署工作流，用于将项目部署到 GitHub Pages
    - 修改 vite.config.ts 中的基础路径配置，以适应生产环境

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/blog/',
+  base: process.env.NODE_ENV === 'production' ? '/blog/' : '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
