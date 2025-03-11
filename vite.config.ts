623994a96d238c37cbb72b40365e7bcd4210b6a7
wuwenx <376679129@qq.com>
2025-03-12 00:00:38 +0800

    build: 修改构建输出目录并更新 GitHub Pages 配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,9 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/blog/',
   build: {
-    outDir: 'docs',
+    outDir: 'dist',
   },
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
