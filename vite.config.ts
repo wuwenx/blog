fd5e65a097302e08518dc9a8f30c3ce91744f58b
wuwenx <376679129@qq.com>
2025-03-11 22:52:24 +0800

    ci: 修改 Vite 配置中的基础路径
    - 将生产环境的基础路径从 '/blog/' 改为 '/'，简化了部署流程
    - 此修改解决了部署在非根路径时遇到的问题，提高了项目的可维护性

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/blog/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [vue(), vueJsx(), vueDevTools()],
   resolve: {
     alias: {
