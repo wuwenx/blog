e17a9368512cbc572fe517eb870ace0503b10f90
wuwenx <376679129@qq.com>
2025-03-12 12:08:21 +0800

    build: 配置环境变量和资源路径

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+console.log('------')
+console.log(process.env.VITE_BASE_PATH)
 // https://vite.dev/config/
 export default defineConfig({
   base: '/blog/',
