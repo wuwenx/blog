b9f125a02969f496944eb31feb9ec851ae1ff543
wuwenx <376679129@qq.com>
2025-03-27 19:22:40 +0800

    fix:优化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+
+// 397df97ffc31f7c4fd3e74f2bb5fd00d66dac0fea86a87754f6dd83dd64f19be
+// const METABASE_JWT_SHARED_SECRET =
+//   '397df97ffc31f7c4fd3e74f2bb5fd00d66dac0fea86a87754f6dd83dd64f19be'
+// const METABASE_SITE_URL = 'https://your-domain.metabaseapp.com'
+// const url = ref('')
+// owen@stonelink.ai
+// const init = () => {
+//   fetch('/api/users/sso/metabase', {
+//     method: 'POST',
+//     body: JSON.stringify({ id: 1, username: 'admin', password: '123456' }),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   })
+//     .then((response) => response.json())
+//     .then((json) => {
+//       url.value = json.url
+//     })
+// }
+// init()
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- {{ url }}
+    <iframe :src="url" frameborder="0" width="1280" height="600" allowtransparency> </iframe> -->
+    啊啥
   </main>
 </template>
--- vite.config.ts
+++ vite.config.ts
@@ -18,4 +18,21 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    // host: 'mm-admin.com',
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        cookieDomainRewrite: 'localhost',
+        rewrite: (path) => path.replace(/^\/api/, ''), // 可选：重写路径，去掉 /api 前缀
+        // headers: {
+        //   authority: 'http://localhost:3000/',
+        //   'x-forwarded-host': 'http://localhost:3000/',
+        //   Referer: 'http://localhost:3000/',
+        // },
+      },
+    },
+  },
 })
